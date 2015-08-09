03bd49e3ba61bcfcdd56c6ac1cf04dc087693ad0
Ryan Hanna <hanna.ryan@gmail.com>
2015-08-09 17:38:32 +0100

    Use router module

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var http = require('http');
+var finalhandler = require('finalhandler');
 var router = require('./lib/router');
 
 var server = http.createServer();
 
-server.on('request', router);
+server.on('request', function(req, res){
+	router(req, res, finalhandler(req, res));
+});
 
 server.listen(process.env.PORT || 3030);
