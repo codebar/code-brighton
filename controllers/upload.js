51487241acd40cf19c2b625e61f22dba49a038d0
Fabien O'Carroll <fabien@allou.is>
2015-08-11 21:02:09 +0100

    Use fstream to write zipped entries to public folder
    We move each folder 'up' a directory to avoid double nesting of directories
    this enables us to use a simple static server rather than custom logic looking
    index.html files

--- controllers/upload.js
+++ controllers/upload.js
@@ -2,15 +2,33 @@ var path = require('path');
 var Busboy = require('busboy');
 var unzip = require('unzip');
 var uuid = require('node-uuid');
+var fstream = require('fstream');
 
 exports.post = function(req, res){
 	var busboy = new Busboy({headers: req.headers});
 	busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
 		var savePath = path.resolve(__dirname + '/../public/' + uuid.v4());
-		file.pipe(unzip.Extract({path: savePath}));
+
+		file.pipe(unzip.Parse()).on('entry', function(entry){
+
+            var entryPathNormalised = entry.path.split('/').slice(1).join('/');
+
+            if (!entryPathNormalised) {
+                return entry.autodrain();
+            }
+
+            var uploadPath = savePath + '/' + entryPathNormalised;
+            var uploadStream = fstream.Writer(uploadPath);
+
+            if (entry.type === 'File') {
+                entry.pipe(uploadStream);
+            } else {
+                entry.autodrain();
+            }
+        });
+
 	});
 	busboy.on('finish', function() {
-		console.log('Done parsing form!');
 		res.writeHead(303, { Connection: 'close', Location: '/' });
 		res.end();
 	});
